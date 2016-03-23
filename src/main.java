d6a6326bc4942ebae4c05f3d30b1b1c9e0d7de29
Cédric Eloundou <Cdric Eloundou>
2016-03-23 23:28:49 +0100

    Mise en place pour jk.com

--- src/main.java
+++ src/main.java
@@ -17,13 +17,18 @@ public class main {
 
 	public static void main(String[] args) throws Exception {
 		
-		JeanKevin jk = JeanKevin.connexion("jk1", "yolo");
-		System.out.println(jk);
-		File img = new File("avatar.jpg");
-		if(jk.ajouterPhotoProfil(img)){
-			jk.ajouterPhotoProfil(img);
-			System.out.println(jk.selectionnerTousAvatars());
-		}
+//		JeanKevin jk = JeanKevin.connexion("jk1", "yolo");
+//		System.out.println(jk);
+//		File img = new File("avatar.jpg");
+//		if(jk.ajouterPhotoProfil(img)){
+//			jk.ajouterPhotoProfil(img);
+//			System.out.println(jk.selectionnerTousAvatars());
+//		}
+
+//		RequeteServeur.executerRequete(Niveau1.JeanKevin, Niveau2.existe, 
+//				new JSONArray(new String[]{"j-k2"}));
+		RequeteServeur.executerRequete(Niveau1.JeanKevin, Niveau2.modifierMail, 
+				new JSONArray(new String[]{"j-k2", "pass", "newgamer1396@gmail.com"}));
 	}
 
 }
--- src/repo/JeanKevin.java
+++ src/repo/JeanKevin.java
@@ -146,16 +146,15 @@ public class JeanKevin {
 	 */
 	public static ArrayList<JeanKevin> rechercher(String motsCles){
 		try{
-			ReponseServeur reponse = new ;
 			//On parse les mots clés en les séparatns à chaque espace
 			String[] mots = motsCles.split(" ");
-			
+			JSONArray jks = new JSONArray();
 			//Pour chaque mot clé récupéré on fait une recherche
 			for(String mot : mots){
 				ReponseServeur r = RequeteServeur.executerRequete(Niveau1.JeanKevin, Niveau2.rechercher,
 						new JSONArray(new String[]{mot}));
 				if(r.estOK()){
-					
+					jks.put(r.getCorps());
 				}
 			}
 		} catch (JSONException e){e.printStackTrace();}
